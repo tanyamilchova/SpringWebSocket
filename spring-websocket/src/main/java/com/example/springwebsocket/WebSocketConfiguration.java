a90410dcecfb02b40c14a42c85ecf9aabc650ab4
Tanya Milchova <margot11margot11@gmail.com>
2024-03-08 11:18:04 +0200

    Spring Web Socket

---
+++ spring-websocket/src/main/java/com/example/springwebsocket/WebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.springwebsocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp-endpoint")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+}
